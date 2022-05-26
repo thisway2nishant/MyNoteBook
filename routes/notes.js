d3d4fc1ad54382aa881e8b4cd252159607bb102d
thisway2nishant <95668425+thisway2nishant@users.noreply.github.com>
2022-05-26 10:48:56 +0530

    Created new endpoin for deleting a note for specific user

--- routes/notes.js
+++ routes/notes.js
@@ -39,19 +39,39 @@ router.post('/addNote', [
 
 router.put('/updateNote/:id', fetchuser, async (req, res)=>{
   const {title, description, tag} = req.body;
-
+try {
   const newNote = {};
   if(title){newNote.title = title;}
   if(description){newNote.description = description;}
   if(tag){newNote.tag = tag;}
 
-  let note = Note.findById(req.params.id);
+  let note = await Note.findById(req.params.id);
   if(!note){res.status(404).send("Not Found")}
 
   if(note.user.toString() !== req.user.id){{res.status(401).send("Not Allowed")}}
 
-  note = Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+  note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
   res.json(note);
+ } catch(error){
+  console.error(error);
+  res.status(500).send("Some error occured.")
+}
+})
+
+router.delete('/deleteNote/:id', fetchuser, async (req,res)=>{
+  try {
+  let note = await Note.findById(req.params.id);
+  if(!note){res.status(404).send("Not Found")}
+
+  if(note.user.toString() !== req.user.id){{res.status(401).send("Not Allowed")}}
+
+  note = await Note.findByIdAndDelete(req.params.id);
+  res.json({"Success": "Note has been deleted", note: note});
+  }  catch(error){
+    console.error(error);
+    res.status(500).send("Some error occured.")
+  }
+
 })
 
 module.exports = router;
