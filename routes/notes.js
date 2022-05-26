0629f6c96de1f3fd5dec96d129829802c61517fa
thisway2nishant <95668425+thisway2nishant@users.noreply.github.com>
2022-05-26 10:27:29 +0530

    created new endpoint for updating a note for specific user

--- routes/notes.js
+++ routes/notes.js
@@ -37,4 +37,21 @@ router.post('/addNote', [
       }
 })
 
+router.put('/updateNote/:id', fetchuser, async (req, res)=>{
+  const {title, description, tag} = req.body;
+
+  const newNote = {};
+  if(title){newNote.title = title;}
+  if(description){newNote.description = description;}
+  if(tag){newNote.tag = tag;}
+
+  let note = Note.findById(req.params.id);
+  if(!note){res.status(404).send("Not Found")}
+
+  if(note.user.toString() !== req.user.id){{res.status(401).send("Not Allowed")}}
+
+  note = Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new: true});
+  res.json(note);
+})
+
 module.exports = router;
