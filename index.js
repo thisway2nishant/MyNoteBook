93e27a8b459597d24b73f274b88bcd9a17eeb538
thisway2nishant <95668425+thisway2nishant@users.noreply.github.com>
2022-06-02 17:28:35 +0530

    port number changed, length validation added to tag in add note

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const connectToMongo  = require('./db.js');
+var cors = require('cors');
 const express = require('express');
 
 connectToMongo();  // connected to database
 const app = express()
 const port = 5000
 
+app.use(cors());
 app.use(express.json()); 
 
 app.use('/api/auth', require('./routes/auth')); //Authentication endpoints.
--- routes/notes.js
+++ routes/notes.js
@@ -18,8 +18,9 @@ router.get('/getNotes', fetchuser, async (req, res)=>{
 
 // POST: Adding a new note by user.Endpoint: /api/notes/addNote. Login Required
 router.post('/addNote', [
-    body("title", "Title must be at least 3 characters long").isLength({ min: 3 }),
-    body("description", "Description must be at least 6 characters long").isLength({ min: 6 }),
+    body("title", "Title must be at least 5 characters long").isLength({ min: 5 }),
+    body("tag", "Tag must be at least 3 characters long").isLength({ min: 3 }),
+    body("description", "Description must be at least 5 characters long").isLength({ min: 5 }),
   ], fetchuser, async (req, res)=>{
 
     // Finds errors IN VALIDATION and returns bad request.
